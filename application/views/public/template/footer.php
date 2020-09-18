fd1bfcdb63c95c042841b82e198adb7b4f869799
Gian Nugraha <giannugraha76@gmail.com>
2020-09-18 16:31:58 +0700

    whatsapp

--- application/views/public/template/footer.php
+++ application/views/public/template/footer.php
@@ -1,3 +1,23 @@
+<style >
+  .float{
+  position:fixed;
+  width:60px;
+  height:60px;
+  bottom:80px;
+  right:50px;
+  background-color:#25d366;
+  color:#FFF;
+  border-radius:50px;
+  text-align:center;
+  font-size:30px;
+  box-shadow: 2px 2px 3px #999;
+  z-index:100;
+}
+
+.my-float{
+  margin-top:16px;
+}
+</style>
 <!-- footer -->
   <div class="footer">
     <div class="container">
@@ -38,6 +58,9 @@
       </div>
     </div>
   </div>
+  <a href="https://api.whatsapp.com/send?phone=6281295704758&text=Halo Admin, Bisa Bantu Saya?" class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
 
   <div class="footer_socialmedia">
     <div class="container">
