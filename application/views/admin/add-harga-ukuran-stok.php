84e9a3240e2265f55562f47ee269ab1377c3c1b3
ziafadhilah <ziafadhilah98@gmail.com>
2020-10-02 17:01:11 +0700

    tambah (belakang) belum beres

--- application/views/admin/add-harga-ukuran-stok.php
+++ application/views/admin/add-harga-ukuran-stok.php
@@ -12,38 +12,68 @@
           	<div class="row">
           		<div class="col-md-6">
           			<div class="card">
-          				<div class="card-header card-header-primary">
-          					<h4>Tambah Stok, Harga, Ukuran Baru</h4>
-          					<p class="card-category">Hanya untuk harga, ukuran dan stok</p>
-          				</div>
-	          				<div class="card-body">
-	          					<div class="row">
-	          						<div class="col-md-4">
-	          							<div class="form-group">
-	          								<label>Stok</label>
-	          								<input type="text" name="stok" class="form-control">
-	          							</div>
-	          						</div>
-	          					</div>
-	          					<div class="row">
-	          						<div class="col-md-4">
-	          							<div class="form-group">
-	          								<label>Harga</label>
-	          								<input type="text" name="harga" class="form-control">
-	          							</div>
-	          						</div>
-	          					</div>
-	          					<div class="row">
-	          						<div class="col-md-4">
-	          							<div class="form-group">
-	          								<label>Ukuran</label>
-	          								<input type="text" name="ukuran" class="form-control">
-	          							</div>
-	          						</div>
-	          					</div>
+          				<form action="#" method="post">
+	          				<div class="card-header card-header-primary">
+	          					<h4>Tambah Stok, Harga, Ukuran</h4>
+	          					<p class="card-category">Hanya untuk harga, ukuran dan stok</p>
 	          				</div>
+		          				<div class="card-body">
+		          					<div class="row">
+		          						<div class="col-md-4">
+		          							<div class="form-group">
+		          								<label>Stok</label>
+		          								<input type="text" name="stok" class="form-control">
+		          							</div>
+		          						</div>
+		          					</div>
+		          					<div class="row">
+		          						<div class="col-md-4">
+		          							<div class="form-group">
+		          								<label>Harga</label>
+		          								<input type="text" name="harga" class="form-control">
+		          							</div>
+		          						</div>
+		          					</div>
+		          					<div class="row">
+		          						<div class="col-md-4">
+		          							<div class="form-group">
+		          								<label>Ukuran</label>
+		          								<select class="form-control">
+		          									<option value="1">1 meter</option>
+		          									<option value="2">2 meter</option>
+		          								</select>
+		          							</div>
+		          						</div>
+		          					</div>
+			          				<button type="submit" class="btn btn-primary btn-pull-right">Tambah</button>
+			          				<div class="clearfix"></div>
+		          				</div>
+	          			</form>
           			</div>
           		</div>
+          		<div class="col-md-4">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title">Tambah Ukuran Baru</h4>
+                  <p class="card-category"></p>
+                </div>
+                <div class="card-body">
+                  <!-- <form role="form" action="<?= base_url('admin/proses_add_ukuran') ?>" method="post" > -->
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Ukuran</label>
+                          <input type="text" name="ukuran" class="form-control">
+                        </div>
+                      </div>
+                    </div>
+                    <input type="hidden" value="not-reg" name="jenis">
+                    <input type="submit" value="kirim" class="btn btn-primary pull-right">
+                    <div class="clearfix"></div>
+                  <!-- </form> -->
+                </div>
+              </div>
+            </div>
           		</div>
           	</div>
 	</div>
