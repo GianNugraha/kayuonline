42996a71a12c59150c19e85c8660ee2c07891752
unknown <ariestaaprilio1995@gmail.com>
2020-10-10 13:44:18 +0700

    modifikasi front-end by arman

--- application/views/public/faq.php
+++ application/views/public/faq.php
@@ -47,15 +47,15 @@
         </div>
         <button class="accordion">Apakah total pembayaran saat beli kayu sudah termasuk biaya muat, angkutan, atau cetak dokumen?</button>
         <div class="panel">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Anda tinggal menunggu barang sampai setelah pembayaran</p>
         </div>
         <button class="accordion">Apakah setelah melakukan pembayaran saya harus melakukan konfirmasi?</button>
         <div class="panel">
-            <p>I am text block. Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+            <p>Konfirmasi melalui whatsapp</p>
         </div>
         <button class="accordion">Bagaimana jika setelah melakukan transfer atau pembayaran, saya tidak menerima email konfirmasi?</button>
         <div class="panel">
-            <p>I am text block. Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+            <p>Silahkan hubungi kami melalui whatsapp</p>
         </div>
         <button class="accordion">Produk apa saja yang ditawarkan oleh KayuOnline?</button>
         <div class="panel">
