e4c6791ca386e2974d2f2caa9670f72a4cf3bb8f
ziafadhilah <ziafadhilah98@gmail.com>
2020-10-13 16:42:47 +0700

    modified stok minim

--- application/views/admin/stok-minim.php
+++ application/views/admin/stok-minim.php
@@ -1,8 +1,3 @@
-<?php foreach ($stokminim as $key) {
-	echo "<pre>";
-	print_r($key);
-	echo "</pre>";
-}?>
 <div class="content">
     <div class="container-fluid">
         <?php
@@ -14,7 +9,7 @@
                 </div>
                 <?php } ?>
                 <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-6">
                         <div class="card card-plain">
                             <div class="card-header card-header-primary">
                                 <h4 class="card-title mt-0"> Daftar Produk Stok Minim</h4>
@@ -28,18 +23,14 @@
                                             <th>No</th>
 						                    <th>Produk</th>
 						                    <th>Ukuran</th>
-						                    <th>Harga</th>
 						                    <th>Stok</th>
-						                    <th>Terjual</th>
                                         </thead>
                                         <tbody>
                                         <?php $no=1; foreach ($stokminim as $key) { ?>
-                                        	<td><?= $no;?></td>
-                                            <td><?= $key->product_id;?></td>
-                                            <td><?= $key->product_size_id;?></td>
-                                            <td><?= $key->price;?></td>
-                                            <td><?= $key->stock;?></td>
-                                            <td><?= $key->sold;?></td>
+                                        	<td style="background: red"><?= $no;?></td>
+                                            <td style="background: red"><?= $key->product_id;?></td>
+                                            <td style="background: red"><?= $key->product_size_id;?></td>
+                                            <td style="background: red"><?= $key->stock;?></td>
                                         </tbody>
                                         <?php $no+=1; } ?>
                                         <?php }
