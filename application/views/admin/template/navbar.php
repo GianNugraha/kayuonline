31be5f25103712d6a1bd2431ef68c7aa2a5c8776
ziafadhilah <ziafadhilah98@gmail.com>
2020-10-19 13:36:53 +0700

    memperbaiki link

--- application/views/admin/template/navbar.php
+++ application/views/admin/template/navbar.php
@@ -56,7 +56,7 @@
             </a>
           </li>
           <li class="nav-item <?php if($active=='status-transaksi'){ ?> active <?php } ?>">
-            <a class="nav-link" href="<?php echo base_url('admin/status-transaksi'); ?>">
+            <a class="nav-link" href="<?php echo base_url('admin/daftar-bukti-tf'); ?>">
               <i style="color: green" class="material-icons">money</i>
               <p>Konfirmasi Transaksi</p>
             </a>
